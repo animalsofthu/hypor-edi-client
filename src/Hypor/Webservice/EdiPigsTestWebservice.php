f20334c9d410a8e3af585fa5a84db314f69e3aa3
Juhász Sándor <juhasz.sandor@lonalore.hu>
2021-11-16 14:50:04 +0100

    Regenrated files.

---
+++ src/Hypor/Webservice/EdiPigsTestWebservice.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Hypor\WebService;
+
+class EdiPigsTestWebservice extends \SoapClient
+{
+
+    /**
+     * @var array $classmap The defined classes
+     */
+    private static $classmap = array (
+      'getMessageRequest' => 'Hypor\\WebService\\getMessageRequest',
+      'getMessageResponse' => 'Hypor\\WebService\\getMessageResponse',
+      'EdiMessage' => 'Hypor\\WebService\\EdiMessage',
+      'putMessageResponse' => 'Hypor\\WebService\\putMessageResponse',
+      'getMessageListRequestContainer' => 'Hypor\\WebService\\getMessageListRequestContainer',
+      'TypegetMessageListRequest' => 'Hypor\\WebService\\TypegetMessageListRequest',
+      'TypeMessageListRequestRelation' => 'Hypor\\WebService\\TypeMessageListRequestRelation',
+      'TypeRelationId' => 'Hypor\\WebService\\TypeRelationId',
+      'getMessageListRequestContainerResponse' => 'Hypor\\WebService\\getMessageListRequestContainerResponse',
+      'TypegetMessageListResponse' => 'Hypor\\WebService\\TypegetMessageListResponse',
+      'MessageList' => 'Hypor\\WebService\\MessageList',
+      'TypeStatus' => 'Hypor\\WebService\\TypeStatus',
+      'TypeMessageListResponseMessage' => 'Hypor\\WebService\\TypeMessageListResponseMessage',
+      'getMessageRequestContainer' => 'Hypor\\WebService\\getMessageRequestContainer',
+      'getMessageRequestContainerResponse' => 'Hypor\\WebService\\getMessageRequestContainerResponse',
+      'putMessageRequestContainer' => 'Hypor\\WebService\\putMessageRequestContainer',
+      'TypeputMessageRequest' => 'Hypor\\WebService\\TypeputMessageRequest',
+      'typeEdiMessage' => 'Hypor\\WebService\\typeEdiMessage',
+      'putMessageRequestContainerResponse' => 'Hypor\\WebService\\putMessageRequestContainerResponse',
+      'getExchangeStatusRequestContainer' => 'Hypor\\WebService\\getExchangeStatusRequestContainer',
+      'TypegetExchangeStatusRequest' => 'Hypor\\WebService\\TypegetExchangeStatusRequest',
+      'TypeExchangeStatusRequestRelation' => 'Hypor\\WebService\\TypeExchangeStatusRequestRelation',
+      'getExchangeStatusRequestContainerResponse' => 'Hypor\\WebService\\getExchangeStatusRequestContainerResponse',
+      'TypegetExchangeStatusResponse' => 'Hypor\\WebService\\TypegetExchangeStatusResponse',
+      'TypeExchangeStatus' => 'Hypor\\WebService\\TypeExchangeStatus',
+      'MissingMessages' => 'Hypor\\WebService\\MissingMessages',
+      'AnimalSynchronisations' => 'Hypor\\WebService\\AnimalSynchronisations',
+      'TypeSynchronisationIndication' => 'Hypor\\WebService\\TypeSynchronisationIndication',
+      'TypeAnimal' => 'Hypor\\WebService\\TypeAnimal',
+    );
+
+    /**
+     * @param array $options A array of config values
+     * @param string $wsdl The wsdl file to use
+     */
+    public function __construct(array $options = array(), $wsdl = null)
+    {
+      foreach (self::$classmap as $key => $value) {
+        if (!isset($options['classmap'][$key])) {
+          $options['classmap'][$key] = $value;
+        }
+      }
+      $options = array_merge(array (
+      'login' => 'EDI-Pigs',
+      'password' => '',
+      'trace' => true,
+      'stream_context' => NULL,
+      'features' => 1,
+    ), $options);
+      if (!$wsdl) {
+        $wsdl = __DIR__ . '/../../../EdiPigsTestWebservice.wsdl';
+      }
+      parent::__construct($wsdl, $options);
+    }
+
+    /**
+     * @param getMessageRequestContainer $parameters
+     * @return getMessageRequestContainerResponse
+     */
+    public function getMessageRequestContainer(getMessageRequestContainer $parameters)
+    {
+      return $this->__soapCall('getMessageRequestContainer', array($parameters));
+    }
+
+    /**
+     * @param putMessageRequestContainer $parameters
+     * @return putMessageRequestContainerResponse
+     */
+    public function putMessageRequestContainer(putMessageRequestContainer $parameters)
+    {
+      return $this->__soapCall('putMessageRequestContainer', array($parameters));
+    }
+
+    /**
+     * @param getMessageListRequestContainer $parameters
+     * @return getMessageListRequestContainerResponse
+     */
+    public function getMessageListRequestContainer(getMessageListRequestContainer $parameters)
+    {
+      return $this->__soapCall('getMessageListRequestContainer', array($parameters));
+    }
+
+    /**
+     * @param getExchangeStatusRequestContainer $parameters
+     * @return getExchangeStatusRequestContainerResponse
+     */
+    public function getExchangeStatusRequestContainer(getExchangeStatusRequestContainer $parameters)
+    {
+      return $this->__soapCall('getExchangeStatusRequestContainer', array($parameters));
+    }
+
+}
